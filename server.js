1462def5757a1da86a9802596760b8865b611f54
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-08 13:09:49 -0700

    create dummy data file

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 
+// app config
 const app = express();
 const port = 9000;
 
+// middleware
+
+// DB config
+
+// api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello Word"));
 
+// listen
 app.listen(port, () => console.log(`listen on localhost: ${port}`));
