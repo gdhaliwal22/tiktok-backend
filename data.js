ea8dce4a91be5572963c528a8c2d1f1274872b7c
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-09-08 13:26:14 -0700

    config db

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const data = [
   {
     url:
-      "https://v16m.tiktokcdn.com/4db8d77ebe69e646b313214853bdf21f/5f580eda/video/tos/useast2a/tos-useast2a-ve-0068/232de9c94cfe4a3c8218e6321c7eb8c6/?a: 1233&br: 3258&bt: 1629&cr: 0&cs: 0&cv: 1&dr: 0&ds: 3&er: &l: 2020090817080201018919521654158F34&lr: tiktok_m&mime_type: video_mp4&qs: 0&rc: ajdzOjw5aHdrcDMzZjczM0ApO2U0NDpoN2UzNzlmZWU8aGdwYWNlLm9yMjFfLS00MTZzczQzXjU2LzE1Yi42NGAvXmM6Yw%3D%3D&vl: &vr: ",
+      "https://v16m.tiktokcdn.com/4db8d77ebe69e646b313214853bdf21f/5f580eda/video/tos/useast2a/tos-useast2a-ve-0068/232de9c94cfe4a3c8218e6321c7eb8c6/?a: 1233&br: 3258&bt: 1629&cr: 0&cs: 0&cv: 1&dr: 0&ds: 3&er: &l: 2020090817080201018919521654158F34&lr: tiktok_m&mime_type: video_mp4&qs: 0&rc: ajdzOjw5aHdrcDMzZjczM0ApO2U0NDpoN2UzNzlmZWU8aGdwYWNlLm9yMjFfLS00MTZzczQzXjU2LzE1Yi42NGAvXmM6Yw%3D%3D&vl:&vr:",
     channel: "gdhaliwal22",
     description: "Dogs and their puppy",
     song: "Dogs",
@@ -11,13 +11,13 @@ const data = [
   },
   {
     url:
-      "https://v16m.tiktokcdn.com/4db8d77ebe69e646b313214853bdf21f/5f580eda/video/tos/useast2a/tos-useast2a-ve-0068/232de9c94cfe4a3c8218e6321c7eb8c6/?a: 1233&br: 3258&bt: 1629&cr: 0&cs: 0&cv: 1&dr: 0&ds: 3&er: &l: 2020090817080201018919521654158F34&lr: tiktok_m&mime_type: video_mp4&qs: 0&rc: ajdzOjw5aHdrcDMzZjczM0ApO2U0NDpoN2UzNzlmZWU8aGdwYWNlLm9yMjFfLS00MTZzczQzXjU2LzE1Yi42NGAvXmM6Yw%3D%3D&vl: &vr: ",
-    channel: "gdhaliwal22",
-    description: "Dogs and their puppy",
+      "https://v58.tiktokcdn.com/video/tos/useast2a/tos-useast2a-ve-0068c001/7f4b623d026e46ada3fbfd538f178847/?VExpiration=1599617571&VSignature=-CRPKM3ninPq_st0iuhYpQ&a=1233&br=2686&bt=1343&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009082011520101902182274C223FE6&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=anFkZjptdTd0dDMzODczM0ApZTw7NjU6NTwzN2c1ZzkzZmdwNmNkNS5fMmdfLS0uMTZzcy5jYDQuNC5hNjY1MTZjLjQ6Yw%3D%3D&vl=&vr=",
+    channel: "ethanfan99",
+    description: "Teaching dogs how to close door",
     song: "Dogs",
-    likes: "2343",
-    messages: "3243",
-    shares: "34",
+    likes: "28343",
+    messages: "7343",
+    shares: "374",
   },
 ];
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import Data from "./data.js";
 
 // app config
 const app = express();
@@ -8,9 +9,17 @@ const port = 9000;
 // middleware
 
 // DB config
+const connection_url =
+  "mongodb+srv://admin:password32286@cluster0.tpygo.mongodb.net/tiktok?retryWrites=true&w=majority";
 
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 // api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello Word"));
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
 
 // listen
 app.listen(port, () => console.log(`listen on localhost: ${port}`));
